0118a34fd8ab10dd891ac471f5608bf3232f4796
Anastasia <zagorodnyuk-03@bk.ru>
2023-12-09 03:05:42 +0200

    insatlling FireBase

---
+++ FireBase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDcbxY5PMKB1u9c7qpY-Gd7hbywnafZfo0",
+  authDomain: "salon-booking-771f3.firebaseapp.com",
+  projectId: "salon-booking-771f3",
+  storageBucket: "salon-booking-771f3.appspot.com",
+  messagingSenderId: "1046258768195",
+  appId: "1:1046258768195:web:4319d4b1454051afc3f7f6",
+};
+
+const app = initializeApp(firebaseConfig);
