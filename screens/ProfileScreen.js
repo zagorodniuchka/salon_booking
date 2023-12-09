c266634812f996b66cf2ce83602225a94c21b08d
Anastasia <zagorodnyuk-03@bk.ru>
2023-12-09 04:29:46 +0200

    ProgileScreen

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,14 +1,35 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { auth } from "../FireBase";
+import { onAuthStateChanged } from "firebase/auth";
 
 const ProfileScreen = () => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setUser(user);
+      setLoading(false);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  const username = user.email.split("@")[0];
+  console.log(username);
+
   return (
-    <View>
-      <Text>ProfileScreen</Text>
+    <View
+      style={{ justifyContent: "center", alignItems: "center", marginTop: 400 }}
+    >
+      <Text style={{ color: "#333333", fontSize: 20, fontWeight: 200 }}>
+        Welcome, {username}!
+      </Text>
     </View>
-  )
-}
+  );
+};
 
-export default ProfileScreen
+export default ProfileScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
